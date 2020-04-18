520310320dac84405e190bb37618c16e4619ebda
Moroccan-Ghost <Moroccan-Ghost@users.noreply.github.com>
2020-04-18 23:48:10 +0100

    n

---
+++ tests/quizbd.php
@@ -0,0 +1,40 @@
+<?php
+for ($i=1; $i <=$_GET['qst'] ; $i++) { 
+	
+if (isset($_POST['question'.$i]) && isset($_POST['repcorrques'.$i]) && isset($_POST['rep2ques'.$i]) && isset($_POST['rep3ques'.$i])) {
+	
+	if (empty($_POST['question'.$i]) || empty($_POST['repcorrques'.$i]) || empty($_POST['rep2ques'.$i]) || empty($_POST['rep3ques'.$i])) {
+			echo "emty in ".$i."<br>";
+        }else{
+			echo "cordonnes ".$i."<br>";
+			/*insertion a la base ici */
+			$query = "insert "
+        }
+
+
+}else {
+	echo "not existe ".$i."<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+?>
--- tests/random.php
+++ tests/random.php
@@ -1,12 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
 <?php
-$array = array('ayman','maryame','asar','ema','raed');
+if (isset($_GET['number'])) {
+	$n = $_GET['number'];
+	echo'<form action="quizbd.php?qst='.$n.'" method="post">
+		  titre<input type="text" name="titre"><br><hr>';
+	for ($i=1; $i <=$n ; $i++) { 
+		
+		echo 'question'.$i.'<input type="text" name="question'.$i.'"><br>';
+		echo 'Question correcte <input type="text" name="repcorrques'.$i.'"><br>';
+		echo 'question 2 <input type="text" name="rep2ques'.$i.'"><br>';
+		echo 'question 3 <input type="text" name="rep3ques'.$i.'"><br><hr>';
+	}
+
+	 echo '<input type="submit" value="submit" >';
+echo '</form>';
+}else{
+echo '
+<form>  
+Enter No:<input type="text" id="number" name="number"/><br/>  
+<input type="submit" value="submit" onclick="getcube()"/>  
+</form> 
+';
 
 
-
-shuffle($array);
-foreach ($array as $rep) {
-	echo '<input type="radio" value="'.$rep.'"  name="q"  >'.$rep.'<br>';
-	
 }
 
 ?>
+
+	
+
+<script type="text/javascript">  
+function getcube(){  
+
+var number=document.getElementById("number").value; 
+
+document.location.replace("random.php?"+number);
+
+
+
+}  
+</script> 
+
+ 
+</body>
+
+</html>
