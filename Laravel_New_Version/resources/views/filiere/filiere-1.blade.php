91458d3d5a4038da2a9931996020456e5fff557c
Moroccan-Ghost <Moroccan-Ghost@users.noreply.github.com>
2020-05-29 07:29:16 +0000

    filiere

--- Laravel_New_Version/resources/views/filiere/filiere-1.blade.php
+++ Laravel_New_Version/resources/views/filiere/filiere-1.blade.php
@@ -10,52 +10,47 @@
     filiere
 @endsection
 
-
 @section('content')
     <div class="container">
         <div class="row">
-              {{-- hadi katsed hiya lewla 9bel mn div li class dialha card hya li dayra lmochkil --}}
-          <div class="col-lg-12 col-md-12 col-sm-12"> 
-            <!-- card -->
-            {{-- the first foreach for the departements --}}
-            <?php $inc=0; ?>
-            @while($inc < $dept_nbr)
-                @foreach ($fields as $dept)
-                    
-                    <div class="card" style="margin-top: 100px; background-color: #eeeeee; border-style: none;">
-                        <button class="accordion"><h3 class="titre">{{strtoupper($dept->departement)}}</h3> </button>
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                {{-- CARD --}}
 
-                    {{-- @for ($i = 0; $i < $fil_nbr[$inc]; $i++)
-                    {{$filiere[$inc][$dept->departement][$i]}}<br>
-                    {{$description[$inc][$dept->departement][$i]}}<br><hr>
-                    @endfor --}}
-
-                    @for ($i = 0; $i < $fil_nbr[$inc]; $i++)
-                    
-                                <img src="static/img/index/filiere/{{$filiere[$inc][$dept->departement][$i]}}.png" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$filiere[$inc][$dept->departement][$i]}}</h5>
-                                    <p class="card-text">{{$description[$inc][$dept->departement][$i]}}</p>
-                                    <p class="card-text"><strong>Cours de la filiere : </strong></p>
+                @php
+                    $inc=0;
+                @endphp
+                @while ($inc < $dept_nbr)
+                    @foreach ($fields as $dept)
+                        <div class="card" style="margin-top: 100px; background-color: #eeeeee; border-style: none;">
+                            <button class="accordion"><h3 class="titre">{{strtoupper($dept->departement)}}</h3></button>
+                            
+                            @for ($i = 0; $i < $fil_nbr[$inc]; $i++)
+                                <div class="panel">
+                                    <img src="static/img/index/filiere/{{$filiere[$inc][$dept->departement][$i]}}.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{$filiere[$inc][$dept->departement][$i]}}</h5>
+                                        <p class="card-text">{{$description[$inc][$dept->departement][$i]}}</p>
+                                        <p class="card-text"><strong>Cours de la filiere : </strong></p>
                                         <ul>
                                             <li>cours n1</li>
                                             <li>cours n2</li>
                                             <li>cours n3</li>
                                         </ul>
-                                        <br><hr>
+                                    </div>
                                 </div>
-                            
+                            @endfor
 
-                    @endfor
-                    
-                    
-                    <?php $inc++; ?>
-                @endforeach
-            @endwhile
+                        </div>
+                        @php
+                            $inc++;
+                        @endphp
+                    @endforeach
+                @endwhile
+
+                {{-- END CARD --}}
             </div>
-          </div>
-        </div>     
-<!-- end card -->
+        </div>
+    </div>
 <script src={{ asset("js/site/filiere.js") }}></script>
 
 @endsection
